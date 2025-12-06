60c9da37cc232a56fff77bfa44e86741c00b41fe
aurielyn <aurielyn.dev@gmail.com>
2025-12-06 16:07:39 +0530

    Misc: some changes

--- src/main/java/com/odtheking/mixin/mixins/EffectsInInventoryMixin.java
+++ src/main/java/com/odtheking/mixin/mixins/EffectsInInventoryMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(EffectsInInventory.class)
 public class EffectsInInventoryMixin {
     @Inject(method = "renderEffects(Lnet/minecraft/client/gui/GuiGraphics;II)V", at = @At("HEAD"), cancellable = true)
-    private void odin$onRenderEffects(GuiGraphics context, int mx, int my, CallbackInfo callbackInfo) {
+    private void onRenderEffects(GuiGraphics context, int mx, int my, CallbackInfo callbackInfo) {
         if (RenderOptimizer.getShouldHideStatusEffects()) callbackInfo.cancel();
     }
 }
--- src/main/java/com/odtheking/mixin/mixins/GuiMixin.java
+++ src/main/java/com/odtheking/mixin/mixins/GuiMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Gui.class)
 public class GuiMixin {
     @Inject(method = "renderEffects", at = @At("HEAD"), cancellable = true)
-    private void odin$cancelRenderEffects(GuiGraphics context, DeltaTracker tickCounter, CallbackInfo ci) {
+    private void cancelRenderEffects(GuiGraphics context, DeltaTracker tickCounter, CallbackInfo ci) {
         if (RenderOptimizer.getShouldHideStatusEffects()) ci.cancel();
     }
 
